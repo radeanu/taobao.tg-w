3cbe9c1d7081a643ed5fb9c3d8bf97e53962adea
radeanu <radeanu.vasile.md@gmail.com>
2025-07-21 13:37:54 +0400

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
     <RouterView />
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+const app = window.Telegram.WebApp;
+
+app.setBackgroundColor('red');
+app.setBottomBarColor('blue');
+app.setHeaderColor('orange');
+</script>
 
 <style lang="scss" scoped></style>
