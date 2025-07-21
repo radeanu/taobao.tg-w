52e25d448bcc7e22c4f5946e615dc847088be628
radeanu <radeanu.vasile.md@gmail.com>
2025-07-21 13:50:25 +0400

    aa

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 const app = window.Telegram.WebApp;
+app.ready();
+app.expand();
 app.sendData('Hello');
 
 // app.setHeaderColor('orange');
