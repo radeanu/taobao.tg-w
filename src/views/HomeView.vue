a5bd312c2ce84e21e55c00ab88ea04c269a117d1
radeanu <radeanu.vasile.md@gmail.com>
2025-07-21 16:02:29 +0400

    v8

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <h3>Form example v7</h3>
+        <h3>Form example v8</h3>
 
         <form @submit.prevent>
             <input type="text" name="name" v-model="payload.name" />
@@ -58,6 +58,7 @@ async function handleSubmit() {
 
 <style lang="scss">
 main {
+    padding: 20px 10px;
     height: calc(100% + 1px);
 }
 
