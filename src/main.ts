b9bec44af82b776a67bd76cade7c00d128617f9f
radeanu <radeanu.vasile.md@gmail.com>
2025-08-18 15:43:19 +0400

    add tg vue sdk

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import '@/styles/main.scss';
+import { init } from '@telegram-apps/sdk-vue';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
+init();
+
 const app = createApp(App);
-
 app.use(router);
-
 app.mount('#app');
