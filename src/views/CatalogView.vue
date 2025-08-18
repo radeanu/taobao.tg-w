958b5b93d2fad24588ba1723cab5bc0a3f630fe7
radeanu <radeanu.vasile.md@gmail.com>
2025-08-18 17:35:17 +0400

    update

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,17 +1,47 @@
 <template>
     <main>
-        <CatalogProducts />
+        <div v-if="loader.isLoading.value" class="loading">Загрузка...</div>
 
-        <ShButton class="btn-cart" kind="telegram">{{ cart.length }} </ShButton>
+        <div class="products">
+            <ProductCard v-for="item in products" :key="item.id" :product="item" />
+        </div>
+
+        <ShButton
+            v-if="pagination.offset"
+            label="Загрузить еще"
+            kind="telegram"
+            full-width
+            :class="{
+                'btn-more': true,
+                'btn-hidden': loader.isLoading.value
+            }"
+            @click="fetchMore"
+        />
+
+        <ShButton
+            v-if="!loader.isLoading.value && cart.length"
+            class="btn-cart"
+            kind="telegram"
+        >
+            {{ cart.length }}
+        </ShButton>
     </main>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
 import { ShButton } from '@UI';
+import { useCatalog } from '@/composables/useCatalog';
+import ProductCard from '@/components/ProductCard.vue';
 import { cartStore } from '@/composables/useCartStorage';
-import CatalogProducts from '@/components/CatalogProducts.vue';
 
 const { cart } = cartStore();
+const { loader, products, fetchMore, pagination, fetchProductsCatalog } = useCatalog();
+
+onMounted(async () => {
+    await fetchProductsCatalog();
+});
 </script>
 
 <style lang="scss" scoped>
@@ -24,5 +54,33 @@ main {
     bottom: 20px;
     right: 0;
     margin-left: auto;
+}
+
+.loading {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 2;
+    padding: 10px 20px;
+    text-align: center;
+    color: var(--tg-theme-text-color);
+    background-color: var(--tg-theme-bg-color, var(--color-white));
+}
+
+.products {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: auto;
+    row-gap: 30px;
+    column-gap: 10px;
+}
+
+.btn-more {
+    margin-top: 20px;
+
+    &.btn-hidden {
+        visibility: hidden;
+    }
 }
 </style>
